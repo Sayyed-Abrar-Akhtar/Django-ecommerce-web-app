e97d0ae4b89d74fabddb7976d81d9216c2cdc485
Sayyedabrarakhtar <sayyedabrarakhtar696@gmail.com>
2020-01-23 13:15:48 +0545

    Modern web commit
    On Trend an online clothing store, new django project created

---
+++ ontrendclothing/ontrendclothing/urls.py
@@ -0,0 +1,33 @@
+"""ontrendclothing URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from product.views import productviews
+
+urlpatterns = [
+    path('', productviews.login),
+    path('showproduct', productviews.showproduct),
+    path('addproduct', productviews.addproduct),
+    path('addtype', productviews.addtype),
+    path('addvendor', productviews.addvendor),
+    path('editproduct/<int:id>', productviews.editproduct),
+    path('updateproduct/<int:id>', productviews.updateproduct),
+    path('deleteproduct/<int:id>', productviews.deleteproduct),
+    path('search', productviews.search),
+    path('login', productviews.login),
+    path('entry', productviews.entry),
+    path('login', productviews.login),
+]
